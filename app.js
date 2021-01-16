047a0bdaf6210d9f2f0a449ebb43ec1ab2055d04
joe-ashwell <67185069+joe-ashwell@users.noreply.github.com>
2021-01-16 12:42:24 +0000

    Remove npm stuff and add some counter functionality

--- app.js
+++ app.js
@@ -1,63 +1,39 @@
-// let albumIndexNumber = 1;
-
-// let imageUrlArray = [];
- 
-// function scrollHandler() {
-//     const distanceToBottom = document.body.getBoundingClientRect().bottom;
- 
-//     if (distanceToBottom < document.documentElement.clientHeight + 150) {
-//         getImages();
-//     }
-// }
- 
-// const getImages = () => {
-//     imageUrlArray = [];
-
-//     let imageEndpoint = `https://jsonplaceholder.typicode.com/photos?albumId=${albumIndexNumber}`;
-
-//     fetch(imageEndpoint)
-//         .then(blob => blob.json())
-//         .then(data => imageUrlArray.push(...data))
-//         .then(renderItems);
-    
-//     albumIndexNumber++;
-//     console.log(imageUrlArray);
-// }
-
-// const renderItems = () => {
-//     imageUrlArray.forEach(item => {
-//         const main = document.querySelector('main');
-//         const newDivElement = document.createElement('div');
-//         newDivElement.innerHTML = `<img src="${item.url}">`;
-//         main.append(newDivElement);
-//     })
-// }
-
-let albumIndexNumber = 1;
-
-let imageUrlArray = [];
- 
-function scrollHandler() {
-    const distanceToBottom = document.body.getBoundingClientRect().bottom;
- 
-    if (distanceToBottom < document.documentElement.clientHeight+50) {
-        getImages();
+const scrollHandler = () => {
+    let distanceToBottomOfDocument = document.body.getBoundingClientRect().bottom;
+    let distanceToBottomOfElement = document.documentElement.clientHeight;
+   
+    if (distanceToBottomOfDocument <= distanceToBottomOfElement) {
         console.log('Ive been called');
+        getImage();
     }
 }
- 
-const getImages = () => {
-    imageUrlArray = [];
 
+
+const getImage = () => {
     let imageEndpoint = `https://source.unsplash.com/random?sig=${Math.random()}`;
-
     const main = document.querySelector('main');
     const newDivElement = document.createElement('div');
     newDivElement.innerHTML = `<img src="${imageEndpoint}">`;
     main.append(newDivElement);
-    
-    albumIndexNumber++;
-    console.log(imageUrlArray);
 }
 
-window.addEventListener('scroll', scrollHandler);
+const scrollCounter = () => {
+    const startDiv = document.querySelector('div.start');
+    const scrollDistanceSpan = document.querySelector('span.scroll-distance');
+
+    let distanceToTopOfDocumentInPX = document.body.getBoundingClientRect().top;
+    let distanceToTopOfDocumentInCM = -distanceToTopOfDocumentInPX * 0.0264583333;
+
+    scrollDistanceSpan.textContent = `${distanceToTopOfDocumentInCM.toFixed(0)}`;
+    console.log(`in pixels ${distanceToTopOfDocumentInPX}`);
+
+    if (-distanceToTopOfDocumentInPX >= 400 && (!startDiv.classList.contains('fixed-to-top'))) {
+        startDiv.classList.add('fixed-to-top');
+    } else if (-distanceToTopOfDocumentInPX < 400 && (startDiv.classList.contains('fixed-to-top'))) {
+        startDiv.classList.remove('fixed-to-top');
+    }
+}
+
+window.addEventListener('load', scrollHandler);
+window.addEventListener('scroll', _.throttle(scrollHandler, 750));
+window.addEventListener('scroll', scrollCounter);

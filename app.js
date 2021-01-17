5a0c2569e1c6a19b996a3033679489535d96c0f7
joe-ashwell <67185069+joe-ashwell@users.noreply.github.com>
2021-01-17 09:56:11 +0000

    Change image to background image & styling changes

--- app.js
+++ app.js
@@ -1,39 +1,40 @@
 const scrollHandler = () => {
     let distanceToBottomOfDocument = document.body.getBoundingClientRect().bottom;
     let distanceToBottomOfElement = document.documentElement.clientHeight;
-   
+
     if (distanceToBottomOfDocument <= distanceToBottomOfElement) {
-        console.log('Ive been called');
         getImage();
     }
 }
-
 
 const getImage = () => {
     let imageEndpoint = `https://source.unsplash.com/random?sig=${Math.random()}`;
     const main = document.querySelector('main');
     const newDivElement = document.createElement('div');
-    newDivElement.innerHTML = `<img src="${imageEndpoint}">`;
+    newDivElement.classList.add('infin-div');
+    newDivElement.style.backgroundImage = `url("${imageEndpoint}")`;
     main.append(newDivElement);
 }
 
 const scrollCounter = () => {
     const startDiv = document.querySelector('div.start');
     const scrollDistanceSpan = document.querySelector('span.scroll-distance');
+    const header = document.querySelector('h1');
 
     let distanceToTopOfDocumentInPX = document.body.getBoundingClientRect().top;
     let distanceToTopOfDocumentInCM = -distanceToTopOfDocumentInPX * 0.0264583333;
 
     scrollDistanceSpan.textContent = `${distanceToTopOfDocumentInCM.toFixed(0)}`;
-    console.log(`in pixels ${distanceToTopOfDocumentInPX}`);
 
     if (-distanceToTopOfDocumentInPX >= 400 && (!startDiv.classList.contains('fixed-to-top'))) {
         startDiv.classList.add('fixed-to-top');
+        header.style.display = 'none';
     } else if (-distanceToTopOfDocumentInPX < 400 && (startDiv.classList.contains('fixed-to-top'))) {
         startDiv.classList.remove('fixed-to-top');
+        header.style.display = 'block';
     }
 }
 
 window.addEventListener('load', scrollHandler);
-window.addEventListener('scroll', _.throttle(scrollHandler, 750));
+window.addEventListener('scroll', _.throttle(scrollHandler, 500));
 window.addEventListener('scroll', scrollCounter);
